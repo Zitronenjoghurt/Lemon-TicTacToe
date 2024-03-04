d8aaae14582fe3123647a4ba4452310af0652c10
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-03-04 21:20:27 +0100

    add log_moves flag and custom exceptions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lemon_tictactoe",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     license="GNU General Public License v3.0",
     description="A library simplifying the process of embedding a TicTacToe game in your python project.",
