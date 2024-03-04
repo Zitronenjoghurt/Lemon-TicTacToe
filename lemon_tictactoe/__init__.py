2e039b4aa4aad5a26194066a641999be212f1b13
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-03-05 00:41:50 +0100

    expose GameFinished and GameStarted exceptions

--- lemon_tictactoe/__init__.py
+++ lemon_tictactoe/__init__.py
@@ -1,2 +1,2 @@
 from .game.game import Game
-from .exceptions import CellOccupiedException, WrongPlayerException
+from .exceptions import CellOccupiedException, WrongPlayerException, GameFinishedException, GameStartedException
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="lemon_tictactoe",
     python_requires=">=3.9",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(),
     license="GNU General Public License v3.0",
     description="A library simplifying the process of embedding a TicTacToe game in your python project.",
