8dbe8ca6cc97918a346cd9f266ea200f2e6f08ab
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-03-05 00:36:52 +0100

    add required python version

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="lemon_tictactoe",
+    python_requires=">=3.9",
     version="0.2.0",
     packages=find_packages(),
     license="GNU General Public License v3.0",
     description="A library simplifying the process of embedding a TicTacToe game in your python project.",
     author="Zitronenjoghurt",
-    extras_require={'dev': ['pytest']},
+    extras_require={'dev': ['pytest', 'twine', 'wheel']},
     url="https://github.com/Zitronenjoghurt/Lemon-TicTacToe"
 )
