e2ccd03eb7b3565681a360049fb092aaa2e9340b
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-03-05 01:20:39 +0100

    resize grid image

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open('README_pypi.md', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="lemon_tictactoe",
     python_requires=">=3.9",
@@ -7,6 +10,8 @@ setup(
     packages=find_packages(),
     license="GNU General Public License v3.0",
     description="A library simplifying the process of embedding a TicTacToe game in your python project.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="Zitronenjoghurt",
     extras_require={'dev': ['pytest', 'twine', 'wheel']},
     url="https://github.com/Zitronenjoghurt/Lemon-TicTacToe"
